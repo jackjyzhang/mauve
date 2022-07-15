c690d1b7719f92554e7530bef93a0805d8c21ad9
Jack Zhang <jzhan237@jhu.edu>
2022-07-15 14:54:07 -0400

    add temp

--- gen_machine_text/generate_backbone.py
+++ gen_machine_text/generate_backbone.py
@@ -6,15 +6,17 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from argparse import ArgumentParser
 import time
 from tqdm import tqdm
-from constants import BOS_TOKEN, PAD_TOKEN
+from constants import BOS_TOKEN, PAD_TOKEN, EOT_TOKEN
 
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument('--backbone', type=str, required=True, help='load ckpt from file if given')
     parser.add_argument('--output', type=str, default=None)
+    parser.add_argument('--is_finetuned', action='store_true', help='will change special tokens on model + tokenizer')
 
     parser.add_argument('--topk', type=int, default=None, help='top k for sampling')
     parser.add_argument('--topp', type=float, default=None, help='top p for sampling')
+    parser.add_argument('--temp', type=float, default=None, help='temperature for sampling')
     parser.add_argument('--num', type=int, default=10)
 
     parser.add_argument('--max_length', type=int, default=512)
@@ -32,8 +34,11 @@ if __name__ == '__main__':
     torch.manual_seed(args.seed)
 
     tokenizer = AutoTokenizer.from_pretrained(args.backbone)
-    model = AutoModelForCausalLM.from_pretrained(args.backbone, return_dict=True, pad_token_id=tokenizer.encode(PAD_TOKEN)[0]).to(args.device)
+    pad_id = tokenizer.encode(PAD_TOKEN)[0] if args.is_finetuned else tokenizer.encode(EOT_TOKEN)[0]
+    model = AutoModelForCausalLM.from_pretrained(args.backbone, return_dict=True, pad_token_id=pad_id).to(args.device)
     model.eval()
+    prefix = BOS_TOKEN if args.is_finetuned else EOT_TOKEN
+    input_ids = tokenizer.encode(prefix, return_tensors='pt').to(args.device)
 
     if args.output:
         f = open(args.output, 'w')
@@ -43,16 +48,15 @@ if __name__ == '__main__':
     print(f'# model path: {args.backbone}', file=f)
     t1 = time.time()
     for _ in tqdm(range(args.num)):
-        prefix = BOS_TOKEN
-        input_ids = tokenizer.encode(prefix, return_tensors='pt').to(args.device)
         topk_output = model.generate(
             input_ids, 
             do_sample=True, 
             max_length=args.max_length, 
             top_k=args.topk,
-            top_p=args.topp
+            top_p=args.topp,
+            temperature=args.temp
         )
-        print(tokenizer.decode(topk_output[0], skip_special_tokens=not args.show_special_tokens), file=f)
+        print(tokenizer.decode(topk_output[0], skip_special_tokens=not args.show_special_tokens).replace('\n', '\\n'), file=f)
 
     t2 = time.time()
     avg_time = (t2-t1) / args.num
--- gen_machine_text/generate_backbone.sh
+++ gen_machine_text/generate_backbone.sh
@@ -2,22 +2,25 @@
 module load anaconda/2021a; source activate jack
 
 SAVE_DIR=$1 # e.g. "backbone/gpt2_yelp_l200_boseos_ep5_lr0.0001"
-NUM=$2
-# TOPK=$3
-TOPP=$3
+IS_FT=$2
+NUM=$3
+HPARAM_NAME=$4 # topk, topp, temp
+HPARAM_VALUE=$5 # topk: positive int, topp: 0 to 1, temp: >=0
+DIR_PREFIX=$6 # can be empty, should contain model name / dataset (e.g. gpt2small, yelp, etc)
 
-OUTPUT_DIR="gen_n${NUM}_topp"
+OUTPUT_DIR="gen_n${NUM}/${DIR_PREFIX}_${HPARAM_NAME}"
 mkdir -p $OUTPUT_DIR
-# FILE_STR="$(basename $SAVE_DIR)"
-# OUTPUT_PATH="${OUTPUT_DIR}/${FILE_STR}_topk${TOPK}.txt"
-OUTPUT_PATH="${OUTPUT_DIR}/topp${TOPP}.txt"
+OUTPUT_PATH="${OUTPUT_DIR}/${HPARAM_NAME}${HPARAM_VALUE}.txt"
+
+if [[ $IS_FT -eq 1 ]]; then
+    IS_FT_FLAG="--is_finetuned"
+fi
 
 echo "output_path=${OUTPUT_PATH}"
 HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 \
 python generate_backbone.py \
     --backbone $SAVE_DIR \
     --output $OUTPUT_PATH \
-    --num $NUM \
-    --topp $TOPP
-    # --topk $TOPK
+    --num $NUM $IS_FT_FLAG \
+    --$HPARAM_NAME $HPARAM_VALUE
     
